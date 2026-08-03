4fd65ce1c93078d00345f6e13e6fea9f54bb36a5
saverioscagnoli <svscagn@gmail.com>
2026-08-03 12:47:43 +0200

    retained rendering, need to split things

--- engine/src/render/retained.rs
+++ engine/src/render/retained.rs
@@ -1,5 +1,45 @@
-use crate::render::packet::FramePacket;
+use utils::{Handle, SlotMap};
 
-pub struct SceneRef<'a> {
-    pub(crate) packet: &'a mut FramePacket,
+use crate::{
+    Color,
+    render::{camera::Camera, layer::Layer, packet::FramePacket},
+};
+
+#[derive(Debug, Clone, Copy)]
+pub struct ViewConfig {
+    pub camera: Option<Handle<Camera>>,
+    pub enabled: bool,
 }
+
+impl ViewConfig {
+    fn new() -> Self {
+        Self {
+            camera: None,
+            enabled: true,
+        }
+    }
+}
+
+pub struct RenderWorld {
+    pub clear_color: Color,
+    cameras: SlotMap<Camera>,
+    views: [ViewConfig; 3],
+}
+
+impl Default for RenderWorld {
+    fn default() -> Self {
+        Self {
+            clear_color: Color::Black,
+            cameras: SlotMap::new(),
+            views: [ViewConfig::new(); 3],
+        }
+    }
+}
+
+impl RenderWorld {
+    fn resolve(&self, layer: Layer) -> Option<Handle<Camera>> {
+        let view = self.views[layer];
+    }
+}
+
+pub struct SceneRef<'a> {}
